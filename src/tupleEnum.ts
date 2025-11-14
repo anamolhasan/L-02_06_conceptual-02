0b868b6411427af7b220884fee76bcc2775744cc
anamolhasan <mdanamolhasan1998@gmail.com>
2025-11-14 09:43:29 +0600

    update

---
+++ src/tupleEnum.ts
@@ -0,0 +1,5 @@
+// tuple
+const student : [string, number] = ['Anam',456]
+
+// Enum
+enum Role {User, Admin}
---
+++ src/typeAliasInterface.ts
@@ -0,0 +1,16 @@
+// type alias
+
+type User = {
+    name:string;
+    roll:number;
+    isActive:true;
+}
+
+
+// type interface
+
+interface Parson {
+    name:string;
+    age:number;
+    isActive:false
+}
